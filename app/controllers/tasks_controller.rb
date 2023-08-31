6c5a9fa30ca84a350ab4e7d82f5244d5d032d77a
Raj Chaudhuri <rajch@hotmail.com>
2023-08-31 06:01:56 +0000

    Modified controller and routes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,36 @@
 class TasksController < ApplicationController
+    def index
+        @tasks = Task.all 
+        render json: Helpers::ApiResult.new(@tasks)
+    end
+
+    def show
+        @task = Task.find(params[:id])
+        render json:  Helpers::ApiResult.new(@task)
+    end 
+
+    def create
+        @task = Task.create(
+            description: params[:description],
+            deadline: params[:deadline],
+            completed: false
+        )
+        render json: Helpers::ApiResult.new(@task)
+    end 
+
+    def update
+        @task = Task.find(params[:id])
+        @task.update(
+            description: params[:description],
+            deadline: params[:deadline],
+            completed: params[:completed]
+        )
+        render json: Helpers::ApiResult.new(@task)
+    end 
+
+    def destroy
+        @task = Task.find(params[:id])
+        @task.destroy
+        render json: Helpers::ApiResult.new(@task)
+    end
 end
